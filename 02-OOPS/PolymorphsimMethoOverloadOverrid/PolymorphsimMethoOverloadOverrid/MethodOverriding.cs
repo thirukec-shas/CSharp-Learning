277b488a1d5d777699bed43de666f5bfbc22c8ab
Thiru <thiru.kec@gmail.com>
2026-09-14 00:07:30 +0530

    Add PolymoriphsmConcepts

---
+++ 02-OOPS/PolymorphsimMethoOverloadOverrid/PolymorphsimMethoOverloadOverrid/MethodOverriding.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphsimMethoOverloadOverrid
+{
+  class Animal
+    {
+        public virtual void Sound()
+        {
+            Console.WriteLine("Animal Makes Sound");
+        }
+    }
+
+    class Peacock : Animal
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("The peacock screams");
+        }
+    }
+
+}
---
+++ 02-OOPS/PolymorphsimMethoOverloadOverrid/PolymorphsimMethoOverloadOverrid/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+ namespace PolymorphsimMethoOverloadOverrid
+{
+
+    class Program
+    {
+        public static void Main()
+        {
+            Calculator Calc = new Calculator();
+            Console.WriteLine(Calc.Add(5, 10));
+            Console.WriteLine(Calc.Add(5, 10, 20));
+
+            Animal Ani = new Peacock();
+            Ani.Sound();
+      }
+    }
+}
