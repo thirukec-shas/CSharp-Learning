8c80f63cdd8785949975a39ec26dbb50c8cdab83
Thiru <thiru.kec@gmail.com>
2026-09-13 17:17:05 +0530

    Add OOP and Reverse String examples

---
+++ 02-OOPS/C#OOPS/C#OOPS/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+class Animal
+{
+    public string Name;
+
+    public void Eat()
+    {
+        Console.WriteLine(Name + " is eating");
+
+    }
+    public static void Main(string[] args)
+    {
+        Animal A = new Animal();
+        A.Name = "Cat";
+        A.Eat();
+     
+
+    }
+
+}
