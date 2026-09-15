9ee05fd7f0a4b10ee060af859fc413c522c27696
Thiru <thiru.kec@gmail.com>
2026-09-16 01:26:02 +0530

    Add InterfaceConcepts

---
+++ 02-OOPS/Interfaceconcepts/Interfaceconcepts/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+interface Ilogger
+{
+    void log(string messge);
+
+}
+
+class Filelogger : Ilogger
+{
+    public void log(string message)
+    {
+        Console.WriteLine("Message is : " + message);
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Ilogger ilog = new Filelogger();
+        ilog.log("Application Started");
+    }
+}
