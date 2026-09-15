f500afa3b8e450ceeccffd093f1f6fb94aada152
Thiru <thiru.kec@gmail.com>
2026-09-15 23:44:47 +0530

    Add Abstraction

---
+++ 02-OOPS/Abstraction/Abstraction/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks.Dataflow;
+
+abstract class Bank
+{
+     public abstract void Moneywithdraw();
+}
+
+class ATM : Bank
+{
+    public override void Moneywithdraw()
+    {
+        Console.WriteLine("Money has been withdrawn");
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Bank bb = new ATM();
+        bb.Moneywithdraw();
+    }
+   
+}
